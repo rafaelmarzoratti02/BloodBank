6b01594480f58cca48f56601f14d62531751df7c
rafaelmarzoratti <rafaelmarzoratti02@gmail.com>
2025-07-16 00:27:46 -0300

    feat: Criando estrutura para implementação na camada de application

---
+++ BloodBank.Services.Donations/BloodBank.Services.Donations/BloodBank.Services.Donations.Infra/MessageBus/ProducerConnection.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Services.Donations.Infra.MessageBus;
+
+public class ProducerConnection
+{
+    public IConnection Connection { get; private set; }
+
+    public ProducerConnection(IConnection connection)
+    {
+        Connection = connection;
+    }
+}
---
+++ BloodBank.Services.Donations/BloodBank.Services.Donations/BloodBank.Services.Donations.Infra/MessageBus/RabbitMQClient.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace BloodBank.Services.Donations.Infra.MessageBus;
+
+public class RabbitMQClient : IMessageBusClient
+{
+    private readonly IConnection _connection;
+
+    public RabbitMQClient(ProducerConnection producerConnection)
+    {
+        _connection = producerConnection.Connection;
+    }
+
+    public async Task PublishAsync(object message, string routingKey, string exchange)
+    {
+        var channel = await _connection.CreateChannelAsync();
+
+        var settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        var payload = Newtonsoft.Json.JsonConvert.SerializeObject(message, settings);
+        var body = Encoding.UTF8.GetBytes(payload);
+
+        await channel.ExchangeDeclareAsync(exchange, "topic", true);
+
+        await channel.BasicPublishAsync(exchange, routingKey, body);
+    }
+}
