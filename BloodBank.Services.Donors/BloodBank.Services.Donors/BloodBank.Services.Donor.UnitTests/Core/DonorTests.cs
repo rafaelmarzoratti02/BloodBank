84e3c7ec72cf5908738c8e43a7c02f560b6d3455
rafaelmarzoratti <rafaelmarzoratti02@gmail.com>
2025-07-17 12:36:58 -0300

    Criando estrutura básica para implementação mais aprofundada

---
+++ BloodBank.Services.Donors/BloodBank.Services.Donors/BloodBank.Services.Donor.UnitTests/Core/DonorTests.cs
@@ -0,0 +1,19 @@
+﻿using BloodBank.Services.Donors.Core;
+using BloodBank.Services.Donors.Core.Enums;
+using BloodBank.Services.Donors.Core.ValueObjects;
+
+namespace BloodBank.Services.Donors.UnitTests.Core;
+
+public class DonorTests
+{
+    [Fact]
+    public void DonorIsCreated()
+    {
+        var donor = new Donor("Teste douglas", "teste@email.com", new DateTime(2002, 7, 17, 14, 30, 0), Gender.Male, 76, BloodType.A, RhFactor.Negative, new Address("Rua Caralinhos", "10", "Poa", "RS", "918827"));
+
+
+
+        Assert.True(donor.IsDeleted == false);
+        Assert.False(donor.IsDeleted);
+    }
+}
